096f2cd52f32466f8f341bc46b0dad3aa9633e58
LL L <lll@xeltek-cn.com>
2017-11-30 10:47:57 +0800

    new home.

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,11 +5,17 @@ import styles from './Home.css';
 
 export default class Home extends Component {
   render() {
+    var aStyle = {
+      backgroundColor: 'red',
+      fontSize:'100px'
+    };
+
     return (
-      <div>
+      <div style={{backgroundColor:'aqua'}}>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <h2>适配器号</h2>
+          <div> John Doe </div>
+          <input style={aStyle} type="text" value="CX001" size="8" />
         </div>
       </div>
     );
